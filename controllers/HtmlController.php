88c5e7c4147ea1d14eaa04de387588157d74ba7c
Akhmad Fakhoni Listiyan Dede <akhmadfakhoni@gmail.com>
2015-09-20 13:08:47 +0700

    controller + model + output data ok

--- controllers/HtmlController.php
+++ controllers/HtmlController.php
@@ -17,66 +17,67 @@ class HtmlController extends Controller
     public function actionIndex()
     {
             $html_model = new Html();
-            if($link = $html_model->getLink('230','TK5001','01'))
-                echo $link;
-            else
-                echo 'not found ';
+//            if($link = $html_model->getLink('230','TK5001','01'))
+//                echo $link;
+//            else
+//                echo 'not found ';
 
-        $data = file_get_html($link);
-        $dataPattern = '/([\w\W]+)[\n|\s]+Program Studi\s*:\s*([\w\W]+)[\n|\s]+Semester\s*:\s*(\d+)\/(\d+)[\n|\s]+Kode\/Mata Kuliah*\s*:\s*(\w{6})\s*\/\s*([\w|\W]+),\s*(\d)\s*SKS[\n|\s]+No. Kelas\/Dosen\s*:\s*(\d\d)\s*\/\s*([\w|\s]*)[\n|\s]+-*[\n|\s]+([\w|\W]+)[\n|\s*]Total Peserta = (\d+)/';
-
-        if(preg_match_all($dataPattern,$data,$data_match))
-        {
-            echo 'count = '.count($data_match). '<br>';
-            foreach($data_match[1] as $element)
-                echo $element. '<br>';
-//            print_r($data_match[1]).'<br>';
-            foreach($data_match[2] as $element)
-                echo $element. '<br>';
-//            print_r($data_match[2]).'<br>';
-            foreach($data_match[3] as $element)
-                echo $element. '<br>';
-//            print_r($data_match[3]).'<br>';
-            foreach($data_match[4] as $element)
-                echo $element. '<br>';
-//            print_r($data_match[4]).'<br>';
-            foreach($data_match[5] as $element)
-                echo $element. '<br>';
-//            print_r($data_match[5]).'<br>';
-            foreach($data_match[6] as $element)
-                echo $element. '<br>';
-//            print_r($data_match[6]).'<br>';
-            foreach($data_match[7] as $element)
-                echo $element. '<br>';
-//            print_r($data_match[7]).'<br>';
-            foreach($data_match[8] as $element)
-                echo $element. '<br>';
-//            print_r($data_match[8]).'<br>';
-            foreach($data_match[9] as $element)
-                echo $element. '<br>';
-//            print_r($data_match[9]).'<br>';
-            foreach($data_match[10] as $element)
-            {
-                $regexPeserta = '/\d{3}\s*(\d{8})\s*([a-z|A-Z|\s]+)/';
-                if(preg_match_all($regexPeserta, $element, $pesertaMatch))
-                {
-                    echo 'nama nama <br>';
-                    for($i=0; $i<count($pesertaMatch[1]);$i++)
-                    {
-                        echo $pesertaMatch[1][$i].' '.$pesertaMatch[2][$i].'<br>';
-                    }
-                }
-            }
-
-//            print_r($data_match[10]).'<br>';
-            foreach($data_match[11] as $element)
-                echo $element. '<br>';
-//            print_r($data_match[11]).'<br>';
-        }
-        else
-        {
-            echo 'not match';
-        }
+            $html_model->getData('135','IF4050','01');
+//        $data = file_get_html($link);
+//        $dataPattern = '/([\w\W]+)[\n|\s]+Program Studi\s*:\s*([\w\W]+)[\n|\s]+Semester\s*:\s*(\d+)\/(\d+)[\n|\s]+Kode\/Mata Kuliah*\s*:\s*(\w{6})\s*\/\s*([\w|\W]+),\s*(\d)\s*SKS[\n|\s]+No. Kelas\/Dosen\s*:\s*(\d\d)\s*\/\s*([\w|\s]*)[\n|\s]+-*[\n|\s]+([\w|\W]+)[\n|\s*]Total Peserta = (\d+)/';
+//
+//        if(preg_match_all($dataPattern,$data,$data_match))
+//        {
+//            echo 'count = '.count($data_match). '<br>';
+//            foreach($data_match[1] as $element)
+//                echo $element. '<br>';
+////            print_r($data_match[1]).'<br>';
+//            foreach($data_match[2] as $element)
+//                echo $element. '<br>';
+////            print_r($data_match[2]).'<br>';
+//            foreach($data_match[3] as $element)
+//                echo $element. '<br>';
+////            print_r($data_match[3]).'<br>';
+//            foreach($data_match[4] as $element)
+//                echo $element. '<br>';
+////            print_r($data_match[4]).'<br>';
+//            foreach($data_match[5] as $element)
+//                echo $element. '<br>';
+////            print_r($data_match[5]).'<br>';
+//            foreach($data_match[6] as $element)
+//                echo $element. '<br>';
+////            print_r($data_match[6]).'<br>';
+//            foreach($data_match[7] as $element)
+//                echo $element. '<br>';
+////            print_r($data_match[7]).'<br>';
+//            foreach($data_match[8] as $element)
+//                echo $element. '<br>';
+////            print_r($data_match[8]).'<br>';
+//            foreach($data_match[9] as $element)
+//                echo $element. '<br>';
+////            print_r($data_match[9]).'<br>';
+//            foreach($data_match[10] as $element)
+//            {
+//                $regexPeserta = '/\d{3}\s*(\d{8})\s*([a-z|A-Z|\s]+)/';
+//                if(preg_match_all($regexPeserta, $element, $pesertaMatch))
+//                {
+//                    echo 'nama nama <br>';
+//                    for($i=0; $i<count($pesertaMatch[1]);$i++)
+//                    {
+//                        echo $pesertaMatch[1][$i].' '.$pesertaMatch[2][$i].'<br>';
+//                    }
+//                }
+//            }
+//
+////            print_r($data_match[10]).'<br>';
+//            foreach($data_match[11] as $element)
+//                echo $element. '<br>';
+////            print_r($data_match[11]).'<br>';
+//        }
+//        else
+//        {
+//            echo 'not match';
+//        }
         //echo $data;
 
 
