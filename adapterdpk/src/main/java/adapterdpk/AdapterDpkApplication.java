67a0d9d373f290ba92e21676c35c8c19cc8675a7
hayyuelha <heripottypot@gmail.com>
2015-09-20 13:01:02 +0700

    project initiation

---
+++ adapterdpk/src/main/java/adapterdpk/AdapterDpkApplication.java
@@ -0,0 +1,12 @@
+package adapterdpk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdapterDpkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdapterDpkApplication.class, args);
+    }
+}
