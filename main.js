44d08edb75d33db937e1402920a1f34fedb706c0
Mamat Rahmat <coderbodoh@gmail.com>
2015-09-21 13:47:18 +0700

    test express

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var express = require('express');
+var app = express();
 
-request('https://news.ycombinator.com', function (error, response, html) {
-  if (!error && response.statusCode == 200) {
-    console.log(html);
-  }
+
+app.get('/', function (req, res){
+	request('https://news.ycombinator.com', function (error, response, html) {
+	  if (!error && response.statusCode == 200) {
+	    console.log(html);
+	  }
+	});
 });
+
+app.listen('80');
+console.log('Server is running');
