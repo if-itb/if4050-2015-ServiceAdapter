c27d104ddb8d395386589f038cdf5edfee92c15c
taufiq <taufiqar@ymail.com>
2015-09-21 13:02:44 +0700

    debugging

--- index.js
+++ index.js
@@ -5,45 +5,49 @@ var six = express();
 
 six.set('port', (process.env.PORT || 5000));
 
-six.use(express.static(__dirname + '/public'));
-
-six.set('views', __dirname + '/views');
-six.set('view engine', 'ejs');
 
 six.get('/', function(req, res){
 	kodematkul = req.query.kode;	
 	prodi = req.query.ps;
 	kelas = req.query.kelas;
 
-	url = 'https://six.akademik.itb.ac.id/publik'
+	url = 'https://six.akademik.itb.ac.id/publik/'
 	urlkelas = 'daftarkelas.php?ps='+prodi+'&semester=1&tahun=2015&th_kur=2013'
 
+	console.log(kodematkul);
+	console.log(prodi);
+	console.log(url+urlkelas);
+
 	request (url+urlkelas,function(error, response, html){
-		if (errror && response.statusCode == 200) {
+		if (!error && response.statusCode == 200) {
 			//masuk ke halaman untuk memilih program studi
-			var $ = cheerio.load(html)				
+			var $ = cheerio.load(html);				
 			var text = '';
 
+			//memasukan seluruh informasi mata kuliah ke dalam array text
 			$('ol').children('li').each(function(index){
 				var text = $(this).contents().filter(function(){
-					return this.nodeType=3;
+					return this.nodeType==3;
 				})[0].nodeValue;
 
 
+				//membandingkan kode matakuliah yang dicari dengan yang ada dalam array
 				if(text.substr(0, text.indexOf(" ")).toLowerCase() === kodematkul.toLowerCase()){
 					urlkelas = $(this).find('ul > li:nth-child('+kelas.substr(1,1)+') > a').attr('href');
 
+					console.log(urlkelas);
+
 					request(url+urlkelas, function(error, response, html){
 						//masuk ke halaman untuk memilih kelas
-						var $ = cheerio.load(html)	;
+						var $ = cheerio.load(html);
 						text = $('pre').text();
 
 						metadata_regex = /(.*?)\n(?:Program Studi\s\s:\s)(.*)\n(?:Semester\s\s:\s)(.{1})(?:\/)(.*)\n\n(?:Kode\/Mata Kuliah\s:\s)(.{6})(?:\s\/\s)(.*)(?:,)(\s.)(?:\sSKS)\n(?:No\.\sKelas\/Dosen\s\s:\s)(.{2})(?:\s\/\s)(.*)\n\n(?:-----------------------------------------------------------)\n(?:No\.   NIM         NAMA)\n(?:-----------------------------------------------------------)\n(?:.\n*)*\n(?:-----------------------------------------------------------)\n(?:Total\sPeserta\s=\s)(.{2})/g;
-            			studentnim_regex = /([\d]{8})   (.*)/g;
+            			nim_regex = /([\d]{8})   (.*)/g;
 
             			var result = metadata_regex.exec(text);
 
-            			var jsonHeader = {};
+            			var infoMatkul = {};
 
             			//retrieve informasi di header
             			infoMatkul['fakultas'] = result[1];
@@ -56,14 +60,14 @@ six.get('/', function(req, res){
             			infoMatkul['kelas'] = result[8];
             			infoMatkul['dosen'] = result[9];
             			infoMatkul['jumlahpeserta'] = result[10];
-            			infoMatkul['peserta'] = result[0];
+            			infoMatkul['peserta'] = [];
 
             			//retrieve informasi nama dan nim
             			do {
-            				match = studentnim_regex.exec(text);
+            				match = nim_regex.exec(text);
             				if (match){
-            					jsonHeader['peserta'].push({
-            						nim:match[1],
+            					infoMatkul['peserta'].push({
+            						nim: match[1],
             						nama:match[2].trim()
             					});
             				}
